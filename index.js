6dfb488053ab76a2d32deba61279e9bfceaac9f0
auxzontech <auxzontech@gmail.com>
2025-02-14 01:12:20 +0530

    added some changes on server.js file, cors setup

--- index.js
+++ index.js
@@ -6,16 +6,34 @@ const mongoose = require('mongoose');
 const TreasRouter = require("./Router/treas.routes");
 require('dotenv').config();
 
-app.use(cors({ orgin: "http://localhost:3000" }));
+const allowedOrigins = [
+  "https://treasfurniture.com",
+  "https://www.treasfurniture.com",
+  "https://admin.treasfurniture.com"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/treas", TreasRouter);
 
+app.get("/", (req, res) => {
+  res.send("<h1>Server is running. Check out <a href='https://treasfurniture.com'>Treas Furniture</a></h1>");
+});
 
 mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-app.listen(8000, () => {
-  console.log("server running...");
-});
+  app.listen(4000, () => {
+    console.log("Server running on http://localhost:4000");
+  });
