445c6f4c50c3cae2052a7fe70c221f823da22fe0
auxzon <auxzontech@gmail.com>
2025-03-07 18:10:41 +0530

    Saving changes before pulling

--- index.js
+++ index.js
@@ -6,21 +6,7 @@ const mongoose = require('mongoose');
 const TreasRouter = require("./Router/treas.routes");
 require('dotenv').config();
 
-// const allowedOrigins = [
-//   "https://treasfurniture.com",
-//   "https://www.treasfurniture.com",
-//   "https://admin.treasfurniture.com"
-// ];
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   }
-// }));
 app.use(cors());
 
 // const allowedOrigins = [
@@ -38,18 +24,14 @@ app.use(cors());
 //     }
 //   }
 // }));
-app.use(cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-app.use(express.urlencoded({extended:true}))
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/treas", TreasRouter);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Server is running. Check out <a href='https://treasfurniture.com'>Treas Furniture</a></h1>");
-});
 app.get("/", (req, res) => {
   res.send("<h1>Server is running. Check out <a href='https://treasfurniture.com'>Treas Furniture</a></h1>");
 });
@@ -58,6 +40,6 @@ mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-  app.listen(4000, () => {
-    console.log("Server running on http://localhost:4000");
-  });
+app.listen(4000, () => {
+  console.log("server running...");
+});
